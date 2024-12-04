ba9bbb99ca81356969fad065dd62b0310cfe6ff7
Liplum <Li_plum@outlook.com>
2024-12-05 04:29:28 +0800

    ts

---
+++ js/src/admin/index.ts
@@ -0,0 +1,41 @@
+import app from 'flarum/admin/app';
+
+app.initializers.add('askvortsov/auth-sync', () => {
+  app.extensionData
+    .for('askvortsov-auth-sync')
+    .registerSetting({
+      setting: 'askvortsov-auth-sync.sync_avatar',
+      label: app.translator.trans('askvortsov-auth-sync.admin.labels.sync_avatar'),
+      type: 'boolean'
+    })
+    .registerSetting({
+      setting: 'askvortsov-auth-sync.ignored_avatar',
+      label: app.translator.trans('askvortsov-auth-sync.admin.labels.ignored_avatar'),
+      type: 'text'
+    })
+    .registerSetting({
+      setting: 'askvortsov-auth-sync.stop_avatar_change',
+      label: app.translator.trans('askvortsov-auth-sync.admin.labels.stop_avatar_change'),
+      type: 'boolean'
+    })
+    .registerSetting({
+      setting: 'askvortsov-auth-sync.sync_groups',
+      label: app.translator.trans('askvortsov-auth-sync.admin.labels.sync_groups'),
+      type: 'boolean'
+    })
+    .registerSetting({
+      setting: 'askvortsov-auth-sync.sync_bio',
+      label: app.translator.trans('askvortsov-auth-sync.admin.labels.sync_bio'),
+      type: 'boolean'
+    })
+    .registerSetting({
+      setting: 'askvortsov-auth-sync.stop_bio_change',
+      label: app.translator.trans('askvortsov-auth-sync.admin.labels.stop_bio_change'),
+      type: 'boolean'
+    })
+    .registerSetting({
+      setting: 'askvortsov-auth-sync.sync_masquerade',
+      label: app.translator.trans('askvortsov-auth-sync.admin.labels.sync_masquerade'),
+      type: 'boolean'
+    })
+});
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,29 +1,29 @@
-import { extend, override } from 'flarum/extend';
-import UserCard from 'flarum/components/UserCard';
-
+import UserCard from 'flarum/forum/components/UserCard'
+import app from 'flarum/forum/app'
+import { extend, override } from 'flarum/common/extend'
 
 app.initializers.add('askvortsov/auth-sync', () => {
-    override(UserCard.prototype, 'view', function (original) {
-        if (app.forum.attribute('stopAvatarChange')) {
-            this.attrs.editable = false;
-        }
+  override(UserCard.prototype, 'view', function (original) {
+    if (app.forum.attribute('stopAvatarChange')) {
+      this.attrs.editable = false
+    }
 
-        return original();
-    });
+    return original()
+  })
 
 
-    extend(UserCard.prototype, 'oncreate', function () {
-        if (app.forum.attribute('stopBioChange') && $('.UserBio').hasClass('editable')) {
-            if ($('.UserBio-content').find('.UserBio-placeholder').length !== 0) {
-                var styleTag = $('<style>.item-bio { display: none !important; }</style>')
-                $('html > head').append(styleTag);
-            } else {
-                $('.UserBio').clone()
-                    .off()
-                    .appendTo('.item-bio');
-                $('.UserBio').first().remove();
-                $('.UserBio').removeClass('editable');
-            }
-        }
-    });
-});
+  extend(UserCard.prototype, 'oncreate', function () {
+    if (app.forum.attribute('stopBioChange') && $('.UserBio').hasClass('editable')) {
+      if ($('.UserBio-content').find('.UserBio-placeholder').length !== 0) {
+        var styleTag = $('<style>.item-bio { display: none !important; }</style>')
+        $('html > head').append(styleTag)
+      } else {
+        $('.UserBio').clone()
+          .off()
+          .appendTo('.item-bio')
+        $('.UserBio').first().remove()
+        $('.UserBio').removeClass('editable')
+      }
+    }
+  })
+})
