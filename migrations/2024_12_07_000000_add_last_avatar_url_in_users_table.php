657689d07590198efc1f006227ca90265284bbb0
Liplum <Li_plum@outlook.com>
2024-12-07 04:15:13 +0800

    updated migration

--- migrations/2024_12_07_000000_add_last_avatar_url_in_users_table.php
+++ migrations/2024_12_07_000000_add_last_avatar_url_in_users_table.php
@@ -1,7 +1,20 @@
 <?php
 
-use Flarum\Database\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
 
-return Migration::addColumns('users', [
-  'last_avatar_url' => ['string', 'nullable' => true],
-]);
+return [
+  'up' => function (Builder $schema) {
+    $schema->table('users', function (Blueprint $table) use ($schema) {
+      if (!$schema->hasColumn('users', 'last_avatar_url')) {
+        $table->text('last_avatar_url')->nullable();
+      }
+    });
+  },
+
+  'down' => function (Builder $schema) {
+    $schema->table('users', function (Blueprint $table) {
+      $table->dropColumn('last_avatar_url');
+    });
+  },
+];
